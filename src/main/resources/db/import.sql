aa0812515f51005edb29ff84aba358b4cc39b74b
htl-leonding <t.stuetz@htl-leonding.ac.at>
2023-03-16 09:52:53 +0100

    inserts für vehicle

--- src/main/resources/db/import.sql
+++ src/main/resources/db/import.sql
@@ -1,2 +1,11 @@
+-- PERSON
 insert into x_person (p_id, p_dob, p_name)
 values (nextval('hibernate_sequence'),'2000-01-02','John Doe');
+
+-- VEHICLE
+insert into x_vehicle (v_id, v_brand, v_model)
+values (nextval('hibernate_sequence'),'VW','eGolf');
+insert into x_vehicle (v_id, v_brand, v_model)
+values (nextval('hibernate_sequence'),'Mercedes','C200');
+insert into x_vehicle (v_id, v_brand, v_model)
+values (nextval('hibernate_sequence'),'Hyundai','Tucson');
